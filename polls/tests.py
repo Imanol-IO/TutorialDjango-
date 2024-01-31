358f4d522fcc793963bc31c2cdb9710fe4340c90
Siegfried16 <imanolrko@gmail.com>
2024-01-30 21:44:28 -0600

    Django, parte 7   Personalizar el formulario de administración

--- polls/tests.py
+++ polls/tests.py
@@ -1,11 +1,11 @@
 import datetime
-from django.urls import reverse
 
 from django.test import TestCase
 from django.utils import timezone
 
 from .models import Question
 
+from django.urls import reverse
 
 class QuestionModelTests(TestCase):
     def test_was_published_recently_with_future_question(self):
